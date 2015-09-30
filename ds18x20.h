16177bbb10541a61f8f0eb42f852e7d69b4f73e6
WiseLord <wiselord1983@gmail.com>
2015-09-30 21:38:17 +0300

    Sync DS18x20 driver with ampcontrol project to save some RAM

--- ds18x20.h
+++ ds18x20.h
@@ -24,12 +24,13 @@
 
 #define DS18X20_MAX_DEV				4
 #define DS18X20_SCRATCH_LEN			9
+#define DS18X20_SCRATCH_TEMP_LEN	2
 #define DS18X20_ID_LEN				8
 
 typedef union {
 	int16_t temp;
 	struct {
-		uint8_t sp[DS18X20_SCRATCH_LEN];
+		uint8_t sp[DS18X20_SCRATCH_TEMP_LEN];
 		uint8_t id[DS18X20_ID_LEN];
 	};
 } ds18x20Dev;
