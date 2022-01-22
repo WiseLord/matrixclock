60cb247c46fd7f43fa3957f48971f1f8309b6dc1
WiseLord <wiselord1983@gmail.com>
2022-01-22 11:32:08 +0300

    Rework project file structure

--- build_all.sh
+++ build_all.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
+cd src/
+
+MAKEOPTS="-j4 -s"
+
 for LED_DRIVER in HT1632 MAX7219 MAX7219_X3
 do
 	for MCU in atmega168p atmega328p atmega8
 	do
 		make clean
-		make LED_DRIVER=${LED_DRIVER} MCU=${MCU}
+		make ${MAKEOPTS} LED_DRIVER=${LED_DRIVER} MCU=${MCU}
 	done
 done
 
